f3092e2dbb46f6cf12863b395fc72357192183f7
becases <50856444+becases@users.noreply.github.com>
2020-09-20 22:02:43 +0800

    Delete 基本调用演示.bat

--- 基本调用演示.bat
+++
@@ -1,9 +0,0 @@
-@Echo off
-Color F0
-SetLocal EnableDelayedExpansion,EnableExtensions
-title Œƒ±æ∑≠“Î
-:menu
-set /p "wenben=«Î ‰»Î:"
-if not defined wenben exit
-trans -f auto -t zh -q "!wenben!"
-goto menu
